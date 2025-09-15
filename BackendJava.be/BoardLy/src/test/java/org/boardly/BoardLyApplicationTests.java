05b9a3210f5e4f075686fee852766cf8a6b4e66f
Avadhut0007 <92705818+Avadhut0007@users.noreply.github.com>
2025-09-16 00:27:45 +0530

    backend added

---
+++ BackendJava.be/BoardLy/src/test/java/org/boardly/BoardLyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.boardly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardLyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
